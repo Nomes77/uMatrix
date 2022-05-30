71298870531c904f4d36885248e68032dc73deb4
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2022-05-30 18:58:31 +0000

    Delete duplicate_tab.js

--- js/duplicate_tab.js
+++
@@ -1,5 +0,0 @@
-chrome.commands.onCommand.addListener(function(command) {
-  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-    browser.tabs.duplicate(tabs[0].id);
-    })
-});
